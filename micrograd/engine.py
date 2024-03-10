6290c1417780526267fb651812fa6a17315bcfae
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-10 10:16:47 +0000

    Fixed backward function in engine.py and finished loss function

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -4,7 +4,6 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-        # internal variables for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
         self._op =  _op
@@ -15,12 +14,12 @@ class Value:
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
+
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
-            # y = 2x + d
-            # dy / dx = 2
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
         out._backward = _backward
+
         return out
     
     def __mul__(self, other):
@@ -30,14 +29,17 @@ class Value:
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
+
         return out
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers"
         out = Value(self.data ** other, (self, ), f'**{other}')
+
         def _backward():
             self.grad += (other * (self.data ** (other - 1)) * out.grad)
         out._backward = _backward
+
         return out
     
     def __neg__(self): # -self
@@ -65,27 +67,35 @@ class Value:
         x = self.data
         t = (math.exp(2*x)-1) / (math.exp(2*x)+1)
         out = Value(t, (self, ), 'tanh')
+
         def _backward():
             self.grad += (1-t**2) * out.grad
+        out._backward = _backward
+
         return out
 
     def exp(self):
         x = self.data
         out = Value(math.exp(x), (self, ), 'exp')
+
         def _backward():
             self.grad += out.data * out.grad
         out._backward = _backward
+
         return out
     
     def backward(self):
+        # uses topological sort 
         topo = []
         visited = set()
         def build_topo(v):
             if v not in visited:
+                visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
         build_topo(self)
+        
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
--- micrograd/loss.py
+++ micrograd/loss.py
@@ -9,12 +9,14 @@ xs = [
     ]
 ys = [1.0, -1.0, -1.0, 1.0] # predicted targets
 
-for k in range(20):
+for k in range(100):
     # forward pass
     ypred = [n(x) for x in xs]
     loss = sum([(ygt - yout)**2 for ygt, yout in zip(ys, ypred)])
 
     # backward pass
+    for p in n.parameters():
+        p.grad = 0.0
     loss.backward()
 
     # update w grad descent optimisation
@@ -22,3 +24,4 @@ for k in range(20):
         p.data += -0.05 * p.grad
     
     print(k, loss.data)
+print(ypred)
