fe59ee8933ae3e52191b285f91e8556abe6fe736
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-10 13:52:19 +0000

    start to normalise counts to create probability distribution

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -25,8 +25,8 @@ N = torch.zeros((27, 27), dtype=torch.int32)
 # reverse stoi
 itos = {i:s for s,i in stoi.items()} 
 
-for w in words:
-    chs = ['<S>'] + list(w) + ['<E>']
+for w in words[:10]:
+    chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
@@ -38,6 +38,20 @@ for w in words:
 #for i in range(28):
 #    for j in range(28):
 #        chstr = itos[i] + itos[j]
-#        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+# s       plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
 #        plt.text(j, i, N[i, j].item(), ha="center", va="top", color="gray")
 #    plt.axis('off');
+        
+# normalise counts and create probability distribution
+p = N[0].float()
+p = p / p.sum()
+print(p)
+
+# returns generator object which produces pseudo random numbers
+g = torch.Generator().manual_seed(2147483647)
+# returns tensor filled with 3 random numbers between 0 and 1 using generator object
+p = torch.rand(3, generator=g)
+p = p / p.sum()
+print(p)
+# draws samples from input tensor with replacement
+torch.multinomial(p, num_samples=20, replacement=True, generator=g)
