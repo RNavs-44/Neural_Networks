eb4fbdd44014d902cd5c611d678422913f83e45c
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-10 14:19:10 +0000

    vectorized normalisation of rows using tensor broadcasting

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -1,6 +1,4 @@
 import torch
-import matplotlib.pyplot as plt
-
 
 words = open('makemore/names.txt', 'r').read().splitlines()
 
@@ -25,35 +23,30 @@ N = torch.zeros((27, 27), dtype=torch.int32)
 # reverse stoi
 itos = {i:s for s,i in stoi.items()} 
 
-#for w in words
-for w in words[:10]:
+for w in words:
     chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
 
-# visualising bigrams using matplotlib
-#plt.figure(figsize=(16,16))
-#plt.imshow(N, cmap='Blues')
-#for i in range(28):
-#    for j in range(28):
-#        chstr = itos[i] + itos[j]
-# s       plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
-#        plt.text(j, i, N[i, j].item(), ha="center", va="top", color="gray")
-#    plt.axis('off');
-        
-
 # returns generator object which produces pseudo random numbers
 g = torch.Generator().manual_seed(2147483647)
 
+P = N.float()
+# P is torch.Size([27, 27])
+# P.sum(1, keepdim=True) is torch.Size([27,1])
+# Broadcasting(whether 2 arrays can be combined in binary operation)
+# Broadcasting semantics:
+# each tensor has at least 1 dimension
+# when iterating over dimension sizes, starting at trailing
+# dimension sizes must either be =, one of them 1, or one doesn't exist
+P /= P.sum(1, keepdim=True)
 for i in range(20):
     out = []
     ix = 0
     while True:
-        # p = N[ix].float()
-        # p = p / p.sum()
-        p = torch.ones(27) / 27.0
+        p = P[ix]
         # draws samples from probability distribution
         ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
         out.append(itos[ix])
