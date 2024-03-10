b657dc581d948fd9f2e9792cb3f56e5aa64c4e2e
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-10 14:27:55 +0000

    created loss function using maximum likelihood estimation

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -34,14 +34,8 @@ for w in words:
 g = torch.Generator().manual_seed(2147483647)
 
 P = N.float()
-# P is torch.Size([27, 27])
-# P.sum(1, keepdim=True) is torch.Size([27,1])
-# Broadcasting(whether 2 arrays can be combined in binary operation)
-# Broadcasting semantics:
-# each tensor has at least 1 dimension
-# when iterating over dimension sizes, starting at trailing
-# dimension sizes must either be =, one of them 1, or one doesn't exist
 P /= P.sum(1, keepdim=True)
+
 for i in range(20):
     out = []
     ix = 0
@@ -53,3 +47,23 @@ for i in range(20):
         if ix == 0:
             break
     print(''.join(out))
+
+# creating loss function using maximum likelihood estimation
+# closer probability is to 1 the closer log likelihood is to 0
+log_likelihood = 0.0
+n = 0
+for w in ["arnav"]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+        print(f"{ch1}{ch2}: {prob:.4f} {logprob:.4f}")
+print(f"{log_likelihood=}")
+nll = -log_likelihood
+print(f"{nll=}")
+# we want to minimise average negative log likelihood
+print(f"{nll/n}")
