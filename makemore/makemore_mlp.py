662b541f4a11b4663f3da220bffe6fbe22aef252
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-11 18:36:17 +0000

    started rebuilding dataset for implementing MLP in the language model

---
+++ makemore/makemore_mlp.py
@@ -0,0 +1,28 @@
+import torch
+
+# read in all the words
+words = open("makemore/names.txt", "r").read().splitlines()
+
+# build vocabulary of characters of and mappings to / from integers
+chars = sorted(list(set(''.join(words))))
+stoi = {s: i + 1 for i, s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i: s for s, i in stoi.items()}
+
+# build the dataset
+
+block_size = 3 # context length: how many characters we take to predict next
+x, y = [], []
+
+for w in words[:8]:
+    print(w)
+    context = [0] * block_size
+    for ch in w + '.':
+        ix = stoi[ch]
+        x.append(context)
+        y.append(ix)
+        print(''.join(itos[i] for i in context), '--->', itos[ix])
+        context = context[1:] + [ix] # crop and append
+
+x = torch.tensor(x)
+y = torch.tensor(y)
