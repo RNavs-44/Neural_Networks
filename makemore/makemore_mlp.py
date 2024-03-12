e382e16a82a9b1a611795781b90ca7831196a550
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-12 12:10:20 +0000

    implemented negative log likelihood loss

--- makemore/makemore_mlp.py
+++ makemore/makemore_mlp.py
@@ -16,13 +16,13 @@ block_size = 3 # context length: how many characters we take to predict next
 x, y = [], []
 
 for w in words[:8]:
-    print(w)
+    #print(w)
     context = [0] * block_size
     for ch in w + '.':
         ix = stoi[ch]
         x.append(context)
         y.append(ix)
-        print(''.join(itos[i] for i in context), '--->', itos[ix])
+        #print(''.join(itos[i] for i in context), '--->', itos[ix])
         context = context[1:] + [ix] # crop and append
 
 x = torch.tensor(x)
@@ -35,4 +35,12 @@ b1 = torch.randn(100)
 # transform 32 x 3 x 2 into 32 x 6
 # -1 makes pytorch infer value
 h = torch.tanh(emb.view(-1, 6) @ w1 + b1)
-print(h.shape)
+w2 = torch.randn((100, 27))
+b2 = torch.randn(27)
+logits = h @ w2 + b2
+counts = logits.exp()
+prob = counts / counts.sum(1, keepdims=True)
+# gives current probabilities assigned by this neural network to correct character in seq
+loss = -prob[torch.arange(53), y].log().mean()
+#prob[torch.arange]
+#loss = -probs[torch.arange(y)].log().mean() + 0.01 * (w**2).mean()
