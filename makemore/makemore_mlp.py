49421551d6c9e1fe284399684097f45b0258714a
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-12 12:57:52 +0000

    split data set into training set, dev set and test set (80, 20, 20)

--- makemore/makemore_mlp.py
+++ makemore/makemore_mlp.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import random
 
 # read in all the words
 words = open("makemore/names.txt", "r").read().splitlines()
@@ -10,34 +11,45 @@ stoi = {s: i + 1 for i, s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i: s for s, i in stoi.items()}
 
+
 # build the dataset
+def build_dataset(words):
+    block_size = 3 # context length: how many characters we take to predict next
+    x, y = [], []
+    for w in words:
+        #print(w)
+        context = [0] * block_size
+        for ch in w + '.':
+            ix = stoi[ch]
+            x.append(context)
+            y.append(ix)
+            #print(''.join(itos[i] for i in context), '--->', itos[ix])
+            context = context[1:] + [ix] # crop and append
 
-block_size = 3 # context length: how many characters we take to predict next
-x, y = [], []
+    x = torch.tensor(x)
+    y = torch.tensor(y)
 
-for w in words:
-    #print(w)
-    context = [0] * block_size
-    for ch in w + '.':
-        ix = stoi[ch]
-        x.append(context)
-        y.append(ix)
-        #print(''.join(itos[i] for i in context), '--->', itos[ix])
-        context = context[1:] + [ix] # crop and append
+    return x, y
 
-x = torch.tensor(x)
-y = torch.tensor(y)
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+
+xtr, ytr = build_dataset(words[:n1])
+xdev, ydev = build_dataset(words[n1:n2])
+xts, yts = build_dataset(words[n2:])
 
 g = torch.Generator().manual_seed(214783647) # for reproducibility
 c = torch.randn((27, 2), generator=g)
-w1 = torch.randn((6, 100))
-b1 = torch.randn(100)
-w2 = torch.randn((100, 27))
+w1 = torch.randn((6, 300))
+b1 = torch.randn(300)
+w2 = torch.randn((300, 27))
 b2 = torch.randn(27)
 parameters = [c, w1, b1, w2, b2]
 
 # number of parameters in total
-print(sum(p.nelement() for p in parameters))
+# print(sum(p.nelement() for p in parameters))
 
 for p in parameters:
     p.requires_grad = True
@@ -47,16 +59,17 @@ lrs = 10 ** lre
 
 lri = []
 lossi = []
+stepi = []
 
-for i in range(1000):
+for i in range(70000):
     # minibatch construction
-    ix = torch.randint(0, x.shape[0], (32,))
+    ix = torch.randint(0, xtr.shape[0], (32,))
 
     # forward pass
-    emb = c[x[ix]] # (53, 3, 2)
-    h = torch.tanh(emb.view(-1, 6) @ w1 + b1) # (53, 100)
-    logits = h @ w2 + b2 # (53, 27)
-    loss = F.cross_entropy(logits, y[ix])
+    emb = c[xtr[ix]] # (32, 3, 2)
+    h = torch.tanh(emb.view(-1, 6) @ w1 + b1) # (32, 100)
+    logits = h @ w2 + b2 # (32, 27)
+    loss = F.cross_entropy(logits, ytr[ix])
     # print(loss.item())
 
     # backward pass
@@ -65,19 +78,24 @@ for i in range(1000):
     loss.backward()
 
     # update
-    lr = lrs[i]
+    # lr = lrs[i]
+    lr = 0.1
     for p in parameters:
-        p.data += lr * p.grad
+        p.data += -lr * p.grad
 
     # track stats
-    lri.append(lre[i])
+    # lri.append(lre[i])
     lossi.append(loss.item())
+    stepi.append(i)
 
 print(loss.item())
 
 # evaluate loss on entire data set
-emb = c[x[ix]] # (53, 3, 2)
+emb = c[xdev] # (53, 3, 2)
 h = torch.tanh(emb.view(-1, 6) @ w1 + b1) # (53, 100)
 logits = h @ w2 + b2 # (53, 27)
-loss = F.cross_entropy(logits, y[ix])
+loss = F.cross_entropy(logits, ydev)
 print(loss.item())
+
+# training split, dev / validation split, test split
+# 80%, 10%, 10%
