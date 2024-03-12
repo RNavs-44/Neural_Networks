a5bad26203e1aa9162449e5706aa073b6871ac04
RNAV-44 <147381594+RNAV-44@users.noreply.github.com>
2024-03-12 13:10:48 +0000

    expiremented with larger embedding size

--- makemore/makemore_mlp.py
+++ makemore/makemore_mlp.py
@@ -41,15 +41,15 @@ xdev, ydev = build_dataset(words[n1:n2])
 xts, yts = build_dataset(words[n2:])
 
 g = torch.Generator().manual_seed(214783647) # for reproducibility
-c = torch.randn((27, 2), generator=g)
-w1 = torch.randn((6, 300))
-b1 = torch.randn(300)
-w2 = torch.randn((300, 27))
+c = torch.randn((27, 10), generator=g)
+w1 = torch.randn((30, 200))
+b1 = torch.randn(200)
+w2 = torch.randn((200, 27))
 b2 = torch.randn(27)
 parameters = [c, w1, b1, w2, b2]
 
 # number of parameters in total
-# print(sum(p.nelement() for p in parameters))
+print(sum(p.nelement() for p in parameters))
 
 for p in parameters:
     p.requires_grad = True
@@ -61,13 +61,13 @@ lri = []
 lossi = []
 stepi = []
 
-for i in range(70000):
+for i in range(20000):
     # minibatch construction
     ix = torch.randint(0, xtr.shape[0], (32,))
 
     # forward pass
     emb = c[xtr[ix]] # (32, 3, 2)
-    h = torch.tanh(emb.view(-1, 6) @ w1 + b1) # (32, 100)
+    h = torch.tanh(emb.view(-1, 30) @ w1 + b1) # (32, 100)
     logits = h @ w2 + b2 # (32, 27)
     loss = F.cross_entropy(logits, ytr[ix])
     # print(loss.item())
@@ -79,20 +79,20 @@ for i in range(70000):
 
     # update
     # lr = lrs[i]
-    lr = 0.1
+    lr = 0.1 if i < 100000 else 0.01
     for p in parameters:
         p.data += -lr * p.grad
 
     # track stats
     # lri.append(lre[i])
-    lossi.append(loss.item())
+    lossi.append(loss.log10().item())
     stepi.append(i)
 
 print(loss.item())
 
 # evaluate loss on entire data set
 emb = c[xdev] # (53, 3, 2)
-h = torch.tanh(emb.view(-1, 6) @ w1 + b1) # (53, 100)
+h = torch.tanh(emb.view(-1, 30) @ w1 + b1) # (53, 100)
 logits = h @ w2 + b2 # (53, 27)
 loss = F.cross_entropy(logits, ydev)
 print(loss.item())
